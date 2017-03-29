6677d1fe924338ab522229651fed6ecd6563d57e
Rijk van Zanten <rijkvanzanten@me.com>
2017-03-29 15:30:26 +0200

    add custom font

--- server.js
+++ server.js
@@ -114,6 +114,7 @@ function respond(res, vdom) {
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Polls</title>
     <link rel="stylesheet" href="/static/style.css">
+    <script src="/static/fontfaceobserver.js"></script>
     <script src="/static/index.js" defer></script>
     ${doc}
   `);
--- src/fontfaceobserver.js
+++ src/fontfaceobserver.js
@@ -1,1 +1,6 @@
-console.log('test');
+/* eslint-env browser */
+const FontFaceObserver = require('fontfaceobserver');
+
+const font = new FontFaceObserver('Lobster');
+
+font.load().then(() => document.documentElement.classList.add('fonts-loaded'));
