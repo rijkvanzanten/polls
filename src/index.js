79a4ae0a0a83d141f80af315eaf9ae8b3a24472e
Rijk van Zanten <rijkvanzanten@me.com>
2017-03-30 12:55:23 +0200

    update DOM with vDom based on socket update

--- src/index.js
+++ src/index.js
@@ -1,25 +1,5 @@
 /* eslint-env browser */
-/* global io */
-const socket = io();
 
-io()
-  .on('connect', convertAnchors)
-  .on('update', update);
-
-function convertAnchors() {
-  document.querySelectorAll('a').forEach(addEventListener);
-
-  function addEventListener(a) {
-    a.addEventListener('click', addSocketEmitListener);
-
-    function addSocketEmitListener(e) {
-      socket.emit('vote', e.target.getAttribute('href'));
-      e.preventDefault();
-      return false;
-    }
-  }
-}
-
-function update(data) {
-  console.log(data);
+if (document.querySelector('main').classList.contains('question')) {
+  require('./modules/socket.js')();
 }
