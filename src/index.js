a3f36b0e2fef41db1dc8150fc2d5ccce76d8970b
Rijk van Zanten <rijkvanzanten@me.com>
2017-03-30 12:08:22 +0200

    connect to socket

--- src/index.js
+++ src/index.js
@@ -1,1 +1,15 @@
-() => { console.log('test'); }
+/* eslint-env browser */
+/* global io */
+const socket = io();
+
+socket.on('connect', () => {
+  document.querySelectorAll('a').forEach(a => a.addEventListener('click', e => {
+    socket.emit('vote', e.target.getAttribute('href'));
+    e.preventDefault();
+    return false;
+  }));
+});
+
+socket.on('update', data => {
+  console.log(data);
+});
